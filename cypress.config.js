692f3b5a8b55b1e08907f81cbbf2bf485cf94e40
amandacarolinek <103580250+amandacarolinek@users.noreply.github.com>
2025-10-08 08:01:02 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'ourf75',
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
-  video: true
+ 
+  
 })
