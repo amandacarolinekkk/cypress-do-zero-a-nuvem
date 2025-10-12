70b85c277d19806918ce7bca53636f7d664da53c
amandacarolinek <103580250+amandacarolinek@users.noreply.github.com>
2025-10-12 20:20:55 -0300

    curso completo cypress do zero a nuvem

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,8 +1,9 @@
-it('Testa a pagina da politica de privacidade de forma independente', () => {
+Cypress._.times(3, () => { it('Testa a pagina da politica de privacidade de forma independente', () => {
   cy.visit('./src/privacy.html')
 
   cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
   cy.contains('p', 'Talking About Testing').should('be.visible')
 
 
-}) 
+   }) 
+})
